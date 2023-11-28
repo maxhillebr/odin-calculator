815cacbf44b4dd875410207ef430a31bbbb166e2
Max Hillebrand <max.hillebrand@yahoo.de>
2023-11-28 10:38:24 +0100

    added functions and loops; problem with the 2nd numbers value to finish the calculation

--- script.js
+++ script.js
@@ -24,11 +24,18 @@ function divide(a, b) {
 // 2. A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
 // num1 + currentOperator + num2 -> Evaluate
 
-let num1 = 3;
-let currentOperator = "/";
-let num2 = 2;
+let num1 = "";
+let currentOperator = "";
+let num2 = "";
 
-function evaluate(a, b) {
+// Update the Textcontent
+let displayNumber = document.querySelector(".calc-display");
+
+// store the num1 / num2 values
+let currentNum1 = "";
+let currentNum2 = "";
+
+function evaluate(a, currentOperator, b) {
   if (currentOperator === "+") {
     return add(a, b);
   }
@@ -46,11 +53,32 @@ function evaluate(a, b) {
 
 // Add click event for all number buttons. When clicked it should update the first num1 value.
 
-// Test for a number
+// select number
 let numbers = document.querySelectorAll(".number");
 
+// loop for all .number buttons and add to current number + display it
+
 for (let i = 0; i < numbers.length; i++) {
-  numbers[i].addEventListener("click", function () {
-    return console.log(numbers[i].value);
+  numbers[i].addEventListener("click", function getCurrentNum1() {
+    if (displayNumber.textContent === "0") {
+      displayNumber.textContent = "";
+    }
+    displayNumber.textContent += numbers[i].value;
+    currentNum1 += numbers[i].value;
+  });
+}
+
+// loop for all .operant and store it to currentOperator; change to currentNum2
+let operants = document.querySelectorAll(".operant");
+
+for (let i = 0; i < operants.length; i++) {
+  operants[i].addEventListener("click", function () {
+    for (let i = 0; i < numbers.length; i++) {
+      numbers[i].removeEventListener("click", () => {});
+    }
+    currentOperator = operants[i].value;
+    console.log(currentNum1);
+    console.log(currentOperator);
+    // add new eventlistener to add currentNumber2 value
   });
 }
