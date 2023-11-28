c87eb4741d25d3a69c88f4c47749f4c8f4bba1cc
Max Hillebrand <max.hillebrand@yahoo.de>
2023-11-28 13:16:42 +0100

    added evaluate function, converts add(a,b) values to numbers

--- script.js
+++ script.js
@@ -6,7 +6,8 @@
 // 1. Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
 
 function add(a, b) {
-  return a + b;
+  console.log(Number(a) + Number(b));
+  return Number(a) + Number(b);
 }
 
 function substract(a, b) {
@@ -24,9 +25,7 @@ function divide(a, b) {
 // 2. A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
 // num1 + currentOperator + num2 -> Evaluate
 
-let num1 = "";
 let currentOperator = "";
-let num2 = "";
 
 // Update the Textcontent
 let displayNumber = document.querySelector(".calc-display");
@@ -34,22 +33,6 @@ let displayNumber = document.querySelector(".calc-display");
 // store the num1 / num2 values
 let currentNum1 = "";
 let currentNum2 = "";
-
-function evaluate(a, currentOperator, b) {
-  if (currentOperator === "+") {
-    return add(a, b);
-  }
-  if (currentOperator === "-") {
-    return substract(a, b);
-  }
-  if (currentOperator === "*") {
-    return multiply(a, b);
-  }
-  if (currentOperator === "/") {
-    return divide(a, b);
-  }
-  return console.log("No current operant?!");
-}
 
 // Add click event for all number buttons. When clicked it should update the first num1 value.
 
@@ -78,14 +61,15 @@ for (let i = 0; i < operants.length; i++) {
 }
 
 function getCurrentOperator(event) {
-  // remove events from .numbers
+  // remove / add events from .numbers
   for (let i = 0; i < numbers.length; i++) {
     numbers[i].removeEventListener("click", getCurrentNum1);
-    numbers[i].addEventListener("click", getCurrentNum2); //add for current number 2
+    numbers[i].addEventListener("click", getCurrentNum2);
   }
   currentOperator = event.target.value;
   displayNumber.textContent = "0";
-  console.log(currentOperator);
+  console.log("The CurrentOperator is: " + currentOperator);
+  console.log(typeof currentOperator);
 }
 
 function getCurrentNum2(event) {
@@ -95,4 +79,24 @@ function getCurrentNum2(event) {
   displayNumber.textContent += event.target.value;
   currentNum2 += event.target.value;
   console.log("Current Num 2: " + currentNum2);
+}
+
+// calculate the result
+let buttonEval = document.querySelector(".evaluate");
+buttonEval.addEventListener("click", evaluate);
+
+function evaluate() {
+  if (currentOperator == "+") {
+    return add(currentNum1, currentNum2);
+  }
+  if (currentOperator == "-") {
+    return substract(currentNum1, currentNum2);
+  }
+  if (currentOperator == "*") {
+    return multiply(currentNum1, currentNum2);
+  }
+  if (currentOperator == "/") {
+    return divide(currentNum1, currentNum2);
+  }
+  return console.log("No current operant?!");
 }
