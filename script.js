1c37d9a56338b391bb2cc589cda4c5bcf53d168f
Max Hillebrand <max.hillebrand@yahoo.de>
2023-11-28 08:31:09 +0100

    changed code to match odins hints, with operants and numbers div to add specific click events

--- script.js
+++ script.js
@@ -3,32 +3,54 @@
 // add currentOperant
 // add another number
 
-// -------first idea--------
-let calculation = [];
-let calcDisplay = document.querySelector(".calc-display");
+// 1. Your calculator is going to contain functions for all of the basic math operators you typically find on simple calculators, so start by creating functions for the following items and testing them in your browser’s console.
 
-let buttonAC = document.querySelector(".div1");
-buttonAC.addEventListener("click", () => {
-  calculation = [];
-  return (calcDisplay.textContent = calculation);
-});
-
-// num9
-let button9 = document.querySelector(".div7");
-button9.addEventListener("click", displayNum9);
-
-function displayNum9() {
-  calculation.push(button9.value);
-  console.log(calculation);
-  return (calcDisplay.textContent += 9);
+function add(a, b) {
+  return a + b;
 }
 
-// operant *
-let operantMultiply = document.querySelector(".div8");
-operantMultiply.addEventListener("click", displayMultiply);
+function substract(a, b) {
+  return a - b;
+}
 
-function displayMultiply() {
-  calculation.push(operantMultiply.value);
-  console.log(calculation);
-  return (calcDisplay.textContent += "*");
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+// 2. A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. Create three variables for each of the parts of a calculator operation. Create a variable for the first number, the operator, and the second number. You’ll use these variables to update your display later.
+// num1 + currentOperator + num2 -> Evaluate
+
+let num1 = 3;
+let currentOperator = "/";
+let num2 = 2;
+
+function evaluate(a, b) {
+  if (currentOperator === "+") {
+    return add(a, b);
+  }
+  if (currentOperator === "-") {
+    return substract(a, b);
+  }
+  if (currentOperator === "*") {
+    return multiply(a, b);
+  }
+  if (currentOperator === "/") {
+    return divide(a, b);
+  }
+  return console.log("No current operant?!");
+}
+
+// Add click event for all number buttons. When clicked it should update the first num1 value.
+
+// Test for a number
+let numbers = document.querySelectorAll(".number");
+
+for (let i = 0; i < numbers.length; i++) {
+  numbers[i].addEventListener("click", function () {
+    return console.log(numbers[i].value);
+  });
 }
